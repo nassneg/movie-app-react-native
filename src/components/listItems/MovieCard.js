b59dc74d11dcfa1c3b9c7c687bfb6f02d12a24f0
nassneg <snailfail@yandex.ru>
2022-06-08 21:20:10 -0700

    Build: Movie Component - List Item

---
+++ src/components/listItems/MovieCard.js
@@ -0,0 +1,45 @@
+import {
+  Box,
+  Button,
+  Center,
+  Divider,
+  Heading,
+  Image,
+  Text,
+  VStack,
+  Flex,
+} from "native-base";
+
+const MovieCard = (props) => {
+  const { image, title, navigation, date, url, popularity } = props;
+  return (
+    <Box borderWidth={1} borderRadius="md">
+      <VStack space={1} divider={<Divider />}>
+        <Flex direction="row" align="left" mb="2" mt="2">
+          <Box w="100" h="100" mr="2" ml="3">
+            <Image alt={title} source={{ uri: image }} size={"2xl"} />
+          </Box>
+
+          <Box h="100">
+            <Heading size="xs">Title{title}</Heading>
+            <Text>Popularity:{popularity}</Text>
+            <Text>Release Date:{date}</Text>
+            <Button
+              w="200"
+              onPress={() =>
+                navigation.navigate("Show", {
+                  title,
+                  url,
+                })
+              }
+            >
+              More Details
+            </Button>
+          </Box>
+        </Flex>
+      </VStack>
+    </Box>
+  );
+};
+
+export default MovieCard;
