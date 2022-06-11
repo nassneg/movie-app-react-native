1e2c8d075122734cc26dbb49789c334ed1b7de72
nassneg <snailfail@yandex.ru>
2022-06-10 21:36:16 -0700

    Build: API Fetch to get one movie by ID

--- src/services/moviesAPI.js
+++ src/services/moviesAPI.js
@@ -4,14 +4,21 @@ import { APP_KEY, BASE_URL } from "../config/api_config";
 export const getMovies = async (type, category) => {
   const moviesArr = axios
     .get(`https://api.themoviedb.org/3/${type}/${category}?api_key=${APP_KEY}`)
-    .then((response) => {
-      const movies = response.data.results;
-
-      return movies;
-    })
+    .then((response) => response.data.results)
     .catch((error) => {
       throw error;
     });
 
   return moviesArr;
+};
+
+export const getMovie = async (type, movieId) => {
+  const movie = axios
+    .get(`https://api.themoviedb.org/3/${type}/${movieId}?api_key=${APP_KEY}`)
+    .then((response) => response.data)
+    .catch((error) => {
+      throw error;
+    });
+
+  return movie;
 };
