a4fe5c99190e80be599616846eefaad64709d61c
nassneg <snailfail@yandex.ru>
2022-06-13 15:50:08 -0700

    Fix: Styling Changes

--- src/components/tabs/AppTab.js
+++ src/components/tabs/AppTab.js
@@ -6,7 +6,17 @@ import TV from "./TVTab";
 const Tab = createMaterialTopTabNavigator();
 
 const AppTab = () => (
-  <Tab.Navigator>
+  <Tab.Navigator
+    screenOptions={{
+      tabBarLabelStyle: { fontSize: 12 },
+      tabBarOptions: {
+        activeTintColor: "#e91e63",
+        labelStyle: {
+          fontSize: 12,
+        },
+      },
+    }}
+  >
     <Tab.Screen name="Movies" component={Movies} />
     <Tab.Screen name="Search Results" component={Search} />
     <Tab.Screen name="TV Shows" component={TV} />
--- src/components/tabs/SearchTab.js
+++ src/components/tabs/SearchTab.js
@@ -33,64 +33,71 @@ const Search = () => {
   };
 
   return (
-    <Center>
-      <Box w="4/5" maxW="300" mt="5" mb="6">
-        <Text>Search Movie/TV Show</Text>
-        <Input
-          placeholder="i.e. James Bond, CSI"
-          width="100%"
-          borderRadius="4"
-          py="3"
-          px="1"
-          fontSize="14"
-          mt="3"
-          onChangeText={(value) => {
-            setTitle(value);
-          }}
-          InputLeftElement={
-            <Icon
-              m="2"
-              ml="3"
-              size="6"
-              color="gray.400"
-              as={<MaterialIcons name="search" />}
-            />
-          }
-        />
-        <Text>Choose Search Type</Text>
-        <Flex direction="row">
-          <Select
-            selectedValue={category}
-            minWidth="200"
-            accessibilityLabel="Choose Movies Category"
-            placeholder="Choose Movies Category"
-            _selectedItem={{
-              bg: "teal.600",
-              endIcon: <CheckIcon size="5" />,
+    <>
+      <Center>
+        <Box w="4/5" maxW="300" mt="5" mb="6">
+          <Text>
+            Search Movie/TV Show
+            <Text color="red.700">*</Text>
+          </Text>
+          <Input
+            placeholder="i.e. James Bond, CSI"
+            width="100%"
+            borderRadius="4"
+            py="3"
+            px="1"
+            fontSize="14"
+            mt="3"
+            onChangeText={(value) => {
+              setTitle(value);
             }}
-            mt={1}
-            onValueChange={(itemValue) => {
-              setCategory(itemValue);
-            }}
-          >
-            <Select.Item label="Multi" value="multi" />
-            <Select.Item label="Movie" value="movie" />
-            <Select.Item label="TV" value="tv" />
-          </Select>
+            InputLeftElement={
+              <Icon
+                m="2"
+                ml="3"
+                size="6"
+                color="gray.400"
+                as={<MaterialIcons name="search" />}
+              />
+            }
+          />
+          <Text>
+            Choose Search Type<Text color="red.700">*</Text>
+          </Text>
+          <Flex direction="row">
+            <Select
+              selectedValue={category}
+              minWidth="200"
+              accessibilityLabel="Choose Movies Category"
+              placeholder="Choose Movies Category"
+              _selectedItem={{
+                bg: "teal.600",
+                endIcon: <CheckIcon size="5" />,
+              }}
+              mt={1}
+              onValueChange={(itemValue) => {
+                setCategory(itemValue);
+              }}
+            >
+              <Select.Item label="Multi" value="multi" />
+              <Select.Item label="Movie" value="movie" />
+              <Select.Item label="TV" value="tv" />
+            </Select>
 
-          <Button
-            onPress={() => onPressSearch(category, title)}
-            w="100"
-            ml="4"
-            leftIcon={<Icon as={Ionicons} name="search" size="sm" />}
-          >
-            Search
-          </Button>
-        </Flex>
-        <Text>Please select a search type</Text>
-      </Box>
-      <Center>{isLoading ? <Loading /> : <MovieList data={movies} />}</Center>
-    </Center>
+            <Button
+              onPress={() => onPressSearch(category, title)}
+              w="100"
+              ml="4"
+              leftIcon={<Icon as={Ionicons} name="search" size="sm" />}
+            >
+              Search
+            </Button>
+          </Flex>
+          <Text fontSize="xs">Please select a search type</Text>
+        </Box>
+      </Center>
+      <Box>{isLoading ? <Loading /> : <MovieList data={movies} />}</Box>
+    </>
   );
 };
 
